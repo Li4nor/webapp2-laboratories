20797a5646aa717fc877b9e838fc5ac743de4b4a
Lianor Bagaporo <156329540+Li4nor@users.noreply.github.com>
2024-05-17 21:41:50 +0800

    Add files via upload

---
+++ 02_simple_php_syntax.php
@@ -0,0 +1,52 @@
+<?php
+//Task1
+$name = 'Lianor Bagaporo';
+$age = 23;
+$address = 'San Jose Del Monte, Bulacan';
+$hobbies = ('Dancing, Volleyball');
+$pet_peeve = 'People who are not organize and resourceful';
+
+echo $name, '<br>';
+echo $age, '<br>';
+echo $address, '<br>';
+echo $hobbies, '<br>';
+echo $pet_peeve, '<br>';
+
+
+
+//Task2
+$name = 'Lianor Bagaporo';
+$age = 23;
+$address = 'San Jose Del Monte, Bulacan';
+//$hobbies = ('Dancing, Volleyball');
+//$pet_peeve = 'People who are not organize;
+
+
+
+
+//Task3
+define('NAME', 'Lianor Bagaporo');
+define('AGE', 23);
+define('ADDRESS', 'San Jose Del Monte, Bulacan');
+define('HOBBIES', 'Dancing, Volleyball');
+define('PET_PEEVE', 'People who are not organize and resourceful');
+
+echo NAME, '<br>';
+echo AGE, '<br>';
+echo ADDRESS, '<br>';
+echo HOBBIES, '<br>';
+echo PET_PEEVE, '<br>';
+
+
+
+
+//Task4
+echo '<pre>';
+var_dump (NAME, AGE, ADDRESS, HOBBIES, PET_PEEVE);
+echo '</pre>';
+
+
+//Task5
+/* 
+Whenever I am going home, there will always be a mess and also the people outside are always littering everywhere. 
+*/
